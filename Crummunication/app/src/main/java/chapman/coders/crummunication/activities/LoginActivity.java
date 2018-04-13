358f55f5be0a877c9d90e410f06a7aec145314a4
Thomas Madden <madde120@mail.chapman.edu>
2018-04-13 10:40:36 -0700

    First commit, set up activities and basic models

---
+++ Crummunication/app/src/main/java/chapman/coders/crummunication/activities/LoginActivity.java
@@ -0,0 +1,18 @@
+package chapman.coders.crummunication.activities;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import chapman.coders.crummunication.R;
+
+/**
+ * Created by tommy on 4/13/18.
+ */
+
+public class LoginActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
